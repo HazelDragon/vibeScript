0f961ecb001c6ada767346dd684382eba12e943f
HazelDragon <114524326+HazelDragon@users.noreply.github.com>
2023-11-27 17:02:52 -0500

    Update vibeScript.js

--- vibeScript.js
+++ vibeScript.js
@@ -1,19 +1,31 @@
-import { generateColor, generateColorRGB, generateSecondaryColor } from "@marko19907/string-to-color";
+import { generateColor, generateColorRGB, generateSecondaryColorRGB, generateSecondaryColor } from "@marko19907/string-to-color";
 
 
 // const express = require('express')
 import express from "express"
 const app = express();
-
-
+const vibes = ['intense','reserved','mysterious','bubbly','kind','calm'];
+let x;
 
 const port = process.env.PORT || 3002
 app.listen(port, () => {
     console.log(`example app listening on port ${port}`);
 })
 
-app.get('/Name/:Name', (req,res) => {
-    const nameColor = generateColorRGB(req.params.Name);
-    console.log("hello")
-    res.send('your name color is' + nameColor );
+app.get('/name/:name', (req,res) => {
+    const nameColor = generateColor(req.params.name);
+    const nameColor2 = generateSecondaryColor(req.params.name);
+    if (JSON.stringify(nameColor) < JSON.stringify(nameColor2))
+    {
+        console.log("yas");
+        x = (Math.floor(Math.random() * 3))*2;
+    }
+    else 
+    {
+        console.log("flop");
+        x = Math.floor(Math.random() * 3);
+    }
+    
+    res.send(`${req.params.name} is ${vibes[x]} and colors are ${nameColor} and ${nameColor2}`);
 });
+
